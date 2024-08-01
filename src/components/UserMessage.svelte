8211ea5906bd3cdcb49ed267db77689c011a089d
Kirboyyy <david.caudill@live.de>
2024-08-01 13:25:35 +0200

    whitespace breaking on user messages

--- src/components/UserMessage.svelte
+++ src/components/UserMessage.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-    class="bg-accent-primary text-text-primary p-3 m-2 rounded-lg self-end max-w-3xl break-words"
+    class="bg-accent-primary text-text-primary p-3 m-2 rounded-lg self-end max-w-3xl break-words whitespace-break-spaces"
 >
     {message.content}
 </div>
